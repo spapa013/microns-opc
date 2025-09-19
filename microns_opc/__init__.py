76143b0562fe0d923e9f99b2d74fba1f52bfc32e
spapa013 <stelios@spapa.us>
2025-09-19 15:06:41 -0700

    chore: remove unnecessary comment

--- microns_opc/__init__.py
+++ microns_opc/__init__.py
@@ -1,5 +1,5 @@
 from importlib.metadata import PackageNotFoundError, version
 try:
-    __version__ = version("microns-opc")  # distribution name
+    __version__ = version("microns-opc")
 except PackageNotFoundError:
     __version__ = "0.0.0+local"
